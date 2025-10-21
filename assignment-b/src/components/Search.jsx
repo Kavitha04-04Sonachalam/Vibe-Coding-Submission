82761d9fe2b3b2a09b622caeeb29f1410972d0ce
Kavitha04-04Sonachalam <kavithaannakkili@gmail.com>
2025-10-21 16:24:59 +0530

    Fix assignment-b folder to be normal directory

---
+++ assignment-b/src/components/Search.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Search = ({searchTerm,setSearchTerm}) => {
+  return (
+    <div className="search">
+        <div>
+            <img
+            src="search.svg" alt="Search" />
+            <input
+              type="text"
+              placeholder="Search for a thousands of Movies"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+
+            />
+
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default Search
